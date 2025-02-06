8e5b9d1823027b1da40c6b72e0ae2a026a562055
Erick <erickalpuchehdez@gmail.com>
2025-02-06 11:40:14 -0500

    Prototipo funcional conectado base de datos front y back

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from "vue";
+import api from "./api";
+
+const ejercicios = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await api.get("/ejercicios");
+    ejercicios.value = response.data;
+  } catch (error) {
+    console.error("Error al obtener los ejercicios:", error);
+  }
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Lista de Ejercicios</h1>
+    <ul>
+      <li v-for="ejercicio in ejercicios" :key="ejercicio.id">
+        {{ ejercicio.nombre }} - {{ ejercicio.descripcion }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
